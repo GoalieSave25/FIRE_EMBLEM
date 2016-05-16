383d74e6e79b8bdf7752036f6e8f02259a03c35d
vodkain <vodkapl0x@gmail.com>
2016-05-16 12:26:25 -0500

    Characters Update

--- src/Characters.java
+++ src/Characters.java
@@ -5,18 +5,34 @@ public class Characters
     final Unit[] squad = new Unit [8];
     /** Constructs a Squad object with 8 different units
       */
-    public Squad ()
+    //predetermined-quicksAve
+    public Characters()
     {           
 	// Range is minus 1, so 2 range is actually only 1
 	//movement, hp, strength, range, image, attack type, 			criticalHit ratio
-	squad [0] = new //job class
-	squad [1] = new //job class
-	squad [2] = new //job class
-	squad [3] = new Unit
-	squad [4] = new Unit
-	squad [5] = new Unit
-	squad [6] = new Unit
-	squad [7] = new Unit
+    	squad [0] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    	squad [1] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    	squad [2] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+		squad [3] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+		squad [4] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+		squad [5] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+		squad [6] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+		squad [7] = new Unit(0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+    
+    //undetermined-custom
+    public Characters(Unit unit0, Unit unit1, Unit unit2, Unit unit3, Unit unit4, Unit unit5, Unit unit6, Unit unit7)
+    {           
+	// Range is minus 1, so 2 range is actually only 1
+	//movement, hp, strength, range, image, attack type, 			criticalHit ratio
+    	squad [0] = unit0;
+    	squad [1] = unit1;
+    	squad [2] = unit2;
+		squad [3] = unit3;
+		squad [4] = unit4;
+		squad [5] = unit5;
+		squad [6] = unit6;
+		squad [7] = unit7;
     }
 
     public String toString ()
@@ -32,87 +48,4 @@ public class Characters
     {
 	return squad [unitNumber];
     }
-	
-//
-public class Unit
-{
-//code it to extend stats and return each class with predetermined stat values to create a team?
-//
-
-}
-
-public Unit (int movement, int hp, int strength, int range, int image, int attackType, int criticalHit)
-    {
-	this.movement = movement;
-	this.hp = hp;
-	this.strength = strength;
-	this.range = range;
-	this.image = image;
-	this.attackType = attackType;
-	this.criticalHit = criticalHit;
-    }
-
-
-    /** Determines the picture and returns the values
-      */
-    public String toString ()
-    {
-	return "" + "Movement: " + movement + "   HP:  " + hp +
-	    "\nStrength:  " + strength + "   Range:  " + range;
-    }
-
-
-
-    public int getMovement ()
-    {
-	return movement;
-    }
-
-
-    public int getHP ()
-    {
-	return hp;
-    }
-
-
-    public int getStrength ()
-    {
-	return strength;
-    }
-
-
-    public int getRange ()
-    {
-	return range;
-    }
-
-
-    public int getImageNumber ()
-    {
-	return image;
-    }
-
-
-    public int getAttackType ()
-    {
-	return attackType;
-    }
-
-
-    public int getCriticalHit ()
-    {
-	return criticalHit;
-    }
-
-
-    /** Add one range to a unit
-      * @param rangeCheck   the num
-      */
-    public int addRange (int rangeCheck)
-    {
-	range += rangeCheck;
-	return range;
-    }
-}
-//
 }
--- src/Unit.java
+++ src/Unit.java
@@ -1,4 +1,9 @@
 
-public class Unit {
+public class Unit extends stats
+{
+	public Unit (int HP, int STRENGTH, int MAGIC, int SKILL, int SPEED, int LUCK, int DEFENSE, int RESISTANCE, int MOVEMENT)
+    {
+		super(HP, STRENGTH, MAGIC, SKILL, SPEED, LUCK, DEFENSE, RESISTANCE, MOVEMENT);
+    }
 
 }
