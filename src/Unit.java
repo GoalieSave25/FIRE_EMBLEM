af1c5e1af96cf633be8b1cde91f550712b30f89e
vodkain <vodkapl0x@gmail.com>
2016-05-18 10:38:46 -0500

    Unit Update

--- src/Unit.java
+++ src/Unit.java
@@ -1,9 +1,21 @@
 
 public class Unit extends stats
 {
-	public Unit (int HP, int STRENGTH, int MAGIC, int SKILL, int SPEED, int LUCK, int DEFENSE, int RESISTANCE, int MOVEMENT)
+	Weapon weapon;
+	public Unit (int HP, int STRENGTH, int MAGIC, int SKILL, int SPEED, int LUCK, int DEFENSE, int RESISTANCE, int MOVEMENT, Weapon weapon)
     {
 		super(HP, STRENGTH, MAGIC, SKILL, SPEED, LUCK, DEFENSE, RESISTANCE, MOVEMENT);
     }
-
+	public int getDmg()
+	{
+		return getStrength() + weapon.getDmg();
+	}
+	public Weapon getWeapon()
+	{
+		return weapon;
+	}
+	public int getLuck()
+	{
+		return super.getLuck() + (int) weapon.getCrit();
+	}
 }
