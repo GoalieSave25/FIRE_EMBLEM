4f961dc42b6ad78adf4e33e968c78d88a091aa7c
SonOfTheSea <142824>
2016-05-18 12:31:32 -0500

    Battle (HOPEFULLY FIXED) -AGAIN
    Commit.exe has failed
    twice

--- src/Battle.java
+++ src/Battle.java
@@ -7,27 +7,42 @@ public class Battle
 		this.attack = attack;
 		this.defend = defend;
 	}
+	public void battle()
+	{
+		attack();
+		if(attack.getWeapon().getRng() == defend.getWeapon().getRng())
+		{
+			counterAttack();
+		}
+		if(attack.getSpeed() >= defend.getSpeed() + 5)
+		{
+			attack();
+		}
+	}
 	public void attack()
 	{
-		
+		defend.looseHP(getDamage(attack, defend));
 	}
 	public void counterAttack()
 	{
-		
+		attack.looseHP(getDamage(defend, attack));
 	}
-	public int getDamage()
+	public int getDamage(Unit attacker, Unit defender)
 	{	
-		int hit = (int) (attack.getSkill() + getWeaponTriangleHit(attack.getWeapon(), defend.getWeapon()) - (defend.getSpeed() + defend.getLuck()));
+		int hit = (int) (attacker.getSkill() + getWeaponTriangleHit(attacker.getWeapon(), defender.getWeapon()) - (defender.getSpeed() + defender.getLuck()));
 		if(hit > 100)
 			hit = 100;
 		int randHit = (int) (Math.random() * 100 - 1);
 		if(randHit < hit)
 			return 0;
 		int damage = 0;
-		int crit = getCritChance(attack, defend, attack.getWeapon());
+		int crit = getCritChance(attacker, defender, attacker.getWeapon());
 		int randCrit = (int) (Math.random() * 100 - 1);
-		int weaponDamage = attack.getWeapon().getDmg() + getWeaponTriangleDamage(attack.getWeapon(),  defend.getWeapon());
-		damage = attack.getStrength() + weaponDamage - defend.getDefense();
+		int weaponDamage = attacker.getWeapon().getDmg() + getWeaponTriangleDamage(attacker.getWeapon(),  defender.getWeapon());
+		if(!attack.getWeapon().getName().contains("thunder"))
+			damage = attacker.getStrength() + weaponDamage - defender.getDefense();
+		else
+			damage = attacker.getMagic() + weaponDamage - defender.getResistance();
 		if(randCrit < crit)
 			damage *= 3;		
 		return damage;
