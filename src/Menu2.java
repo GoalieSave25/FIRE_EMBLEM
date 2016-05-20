57e8df51d1c220fcd5571c43c2e3d1a29f1dd7eb
MaddieAckerman <madelyna77@gmail.com>
2016-05-20 12:43:31 -0500

    Menu2 2.1

--- src/Menu2.java
+++ src/Menu2.java
@@ -41,7 +41,7 @@ public class Menu2 extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 641, 466);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(233, 150, 122));
+		contentPane.setBackground(new Color(255, 218, 185));
 		contentPane.setForeground(new Color(248, 248, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -60,34 +60,68 @@ public class Menu2 extends JFrame {
 		contentPane.add(lblChooseACharacter);
 		
 		JButton myrmidon = new JButton(new ImageIcon("1.gif"));
+		myrmidon.setBackground(new Color(210, 180, 140));
 		myrmidon.setBounds(50, 101, 97, 87);
+		/*myrmidon.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				contentPane.removeAll();
+				contentPane.setVisible(false);
+				dispose();
+				EventQueue.invokeLater(new Runnable() 
+				{
+					public void run() 
+					{
+						try 
+						{
+							Main frame = new Main();
+							frame.setVisible(true);
+							frame.setResizable(false);
+							frame.setTitle("GIF Viewer");
+						} 
+						catch (Exception e) 
+						{
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});*/
 		contentPane.add(myrmidon);
 		
 		JButton mage = new JButton(new ImageIcon("2.gif"));
+		mage.setBackground(new Color(210, 180, 140));
 		mage.setBounds(194, 101, 97, 87);
 		contentPane.add(mage);
 		
 		JButton thief = new JButton(new ImageIcon("3.gif"));
+		thief.setBackground(new Color(210, 180, 140));
 		thief.setBounds(338, 101, 97, 87);
 		contentPane.add(thief);
 		
 		JButton cavalier = new JButton(new ImageIcon("4.gif"));
+		cavalier.setBackground(new Color(210, 180, 140));
 		cavalier.setBounds(482, 101, 97, 87);
 		contentPane.add(cavalier);
 		
 		JButton knight = new JButton(new ImageIcon("5.gif"));
+		knight.setBackground(new Color(210, 180, 140));
 		knight.setBounds(50, 242, 97, 87);
 		contentPane.add(knight);
 		
 		JButton fighter = new JButton(new ImageIcon("6.gif"));
+		fighter.setBackground(new Color(210, 180, 140));
 		fighter.setBounds(194, 242, 97, 87);
 		contentPane.add(fighter);
 		
 		JButton mercenary = new JButton(new ImageIcon("7.gif"));
+		mercenary.setBackground(new Color(210, 180, 140));
 		mercenary.setBounds(338, 242, 97, 87);
 		contentPane.add(mercenary);
 		
 		JButton archer = new JButton(new ImageIcon("8.gif"));
+		archer.setBackground(new Color(210, 180, 140));
 		archer.setBounds(482, 242, 97, 87);
 		contentPane.add(archer);
 		
@@ -141,6 +175,7 @@ public class Menu2 extends JFrame {
 		
 		JButton btnBack = new JButton("Return to Main Menu");
 		btnBack.setFont(new Font("Papyrus", Font.PLAIN, 14));
+		btnBack.setBackground(new Color(210, 180, 140));
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) 
 			{
